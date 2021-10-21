7b9c9910f7bfc600fe21e3ae93e4c8a2c5de8b25
JorisVanElk <j.van.elk@sintlucasedu.nl>
2021-10-21 10:38:39 +0200

    Publish

--- scripts/scrollButton.js
+++ scripts/scrollButton.js
@@ -5,24 +5,25 @@ var header2 = document.getElementById("navItem2");
 var header3 = document.getElementById("navItem3");
 var section2 = document.getElementById("section2");
 var section3 = document.getElementById("section3");
-var sec2Title = document.getElementById("section2Title");
-var sec2Content = document.getElementById("section2Content")
+var sec2Title = document.getElementById("sec2Title");
+var sec2Content = document.getElementById("sec2Content")
 
-// function trigger(aT, bT) {
-// 	setTimeout(() => { animate(aT) }, 500);
-// 	setTimeout(() => { animate(bT) }, 650);
-// }
+function trigger(aT, bT) {
+	setTimeout(() => { animate(aT) }, 500);
+	setTimeout(() => { animate(bT) }, 650);
+	console.log(bT);
+}
 
-// function animate(a) {
-// 	a.style.opacity = "100%"
-// 	a.style.transform = "translateY(0px)"
-// }
+function animate(a) {
+	a.style.opacity = "100%"
+	a.style.transform = "translateY(0px)"
+}
 
 // button.addEventListener("click", function(){section2.scrollIntoView({behavior: "smooth", block: "start"})});
 button.addEventListener("click", function(){trigger(sec2Title, sec2Content)});
 header1.addEventListener("click", function(){section1.scrollIntoView({behavior: "smooth", block: "start"})});
-// button2.addEventListener("click", function(){section3.scrollIntoView({behavior: "smooth", block: "start"})});
-// header2.addEventListener("click", function(){trigger(sec2Title, sec2Content)});
+button2.addEventListener("click", function(){section3.scrollIntoView({behavior: "smooth", block: "start"})});
+header2.addEventListener("click", function(){trigger(sec2Title, sec2Content)});
 header2.addEventListener("click", function(){section2.scrollIntoView({behavior: "smooth", block: "start"})});
 header3.addEventListener("click", function(){section3.scrollIntoView({behavior: "smooth", block: "start"})});
 
